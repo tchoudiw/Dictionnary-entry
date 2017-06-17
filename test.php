5b3b713e43d617aa4d6ed01f3cc2edcf1277dad5
RalphPredator <foumbi45@gmail.com>
2017-06-17 23:06:50 +0200

    Update test.php

--- test.php
+++ test.php
@@ -6,12 +6,44 @@
     //convert json object to php associative array
     
 	//var_dump(json_decode($json, true));
-	$parsed_json = json_decode($json);
+	$parsed_json = json_decode($json,true);
 	
-	  
-		$date = $parsed_json->{'dictionnaire'}->{'entry'}[0]->{'cita'}->{'block'}[0]->{'fran'};
+	/*	fonctionne
+	for($i=0;$i<3;$i++)
+	{
+		$date = $parsed_json['dictionnaire']['entry'][0]['cita']['block'][$i]['fran'];
 	
-		echo $date;
+		echo $date.'<br>';
+	}
+	*/
 	
-		
+	/*	fonctionne
+	foreach($parsed_json['dictionnaire']['entry'][0]['cita']['block'][$i] as $value)
+	{
+		echo $value;
+	}
+	*/
+	
+	/*	fonctionne
+	foreach($parsed_json['dictionnaire']['entry'][0]['cita']['block'] as $i)
+	{
+		foreach($i as $value)
+		{
+			echo $value.'<br>';
+		}
+		echo '<br>';
+	}
+	*/
+	
+	foreach($parsed_json['dictionnaire']['entry'][0]['cita'] as $block)
+	{
+		 foreach($block as $i)
+		{
+			foreach($i as $value)
+			{
+				echo $value.'<br>';
+			}
+			echo '<br>';
+		}
+	}
 ?>
